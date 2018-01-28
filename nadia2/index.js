16ac30d00b35ca473245abdac4723d05f903e2b3
Hebah89 <33028225+Hebah89@users.noreply.github.com>
2018-01-28 12:24:14 +0200

    Create index.js

---
+++ nadia2/index.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class web-1528 extends Component {
+	
+  render(){
+	  
+    return (
+      <div className='web-1528'>
+	      <h2 className='demo_text'>nadia2: {this.props.text}</h2>
+      </div>
+    );
+	  
+  }
+}
+
+export default ModuleDemo;
